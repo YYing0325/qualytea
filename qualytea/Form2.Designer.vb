92cd686e9269b0d77441f9582b7ee4612b0264c0
yingyee <yingyee95@hotmail.com>
2017-10-21 16:57:38 +0800

    Added Form2.vb

---
+++ qualytea/Form2.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class cmdlogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(cmdlogin))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.lbusername = New System.Windows.Forms.Label
+        Me.lbpassword = New System.Windows.Forms.Label
+        Me.tbusername = New System.Windows.Forms.TextBox
+        Me.tbpassword = New System.Windows.Forms.TextBox
+        Me.cbrmbr = New System.Windows.Forms.CheckBox
+        Me.btnlogin = New System.Windows.Forms.Button
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(88, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(114, 83)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'lbusername
+        '
+        Me.lbusername.AutoSize = True
+        Me.lbusername.Font = New System.Drawing.Font("Lato", 8.999999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbusername.Location = New System.Drawing.Point(36, 122)
+        Me.lbusername.Name = "lbusername"
+        Me.lbusername.Size = New System.Drawing.Size(63, 15)
+        Me.lbusername.TabIndex = 2
+        Me.lbusername.Text = "Username:"
+        '
+        'lbpassword
+        '
+        Me.lbpassword.AutoSize = True
+        Me.lbpassword.Font = New System.Drawing.Font("Lato", 8.999999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbpassword.Location = New System.Drawing.Point(36, 156)
+        Me.lbpassword.Name = "lbpassword"
+        Me.lbpassword.Size = New System.Drawing.Size(60, 15)
+        Me.lbpassword.TabIndex = 3
+        Me.lbpassword.Text = "Password:"
+        '
+        'tbusername
+        '
+        Me.tbusername.Location = New System.Drawing.Point(97, 119)
+        Me.tbusername.Name = "tbusername"
+        Me.tbusername.Size = New System.Drawing.Size(142, 21)
+        Me.tbusername.TabIndex = 4
+        '
+        'tbpassword
+        '
+        Me.tbpassword.Location = New System.Drawing.Point(97, 153)
+        Me.tbpassword.Name = "tbpassword"
+        Me.tbpassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.tbpassword.Size = New System.Drawing.Size(142, 21)
+        Me.tbpassword.TabIndex = 5
+        '
+        'cbrmbr
+        '
+        Me.cbrmbr.AutoSize = True
+        Me.cbrmbr.Location = New System.Drawing.Point(39, 191)
+        Me.cbrmbr.Name = "cbrmbr"
+        Me.cbrmbr.Size = New System.Drawing.Size(96, 16)
+        Me.cbrmbr.TabIndex = 6
+        Me.cbrmbr.Text = "Remember me!"
+        Me.cbrmbr.UseVisualStyleBackColor = True
+        '
+        'btnlogin
+        '
+        Me.btnlogin.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnlogin.Font = New System.Drawing.Font("Lato", 8.999999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnlogin.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnlogin.Location = New System.Drawing.Point(156, 216)
+        Me.btnlogin.Name = "btnlogin"
+        Me.btnlogin.Size = New System.Drawing.Size(83, 33)
+        Me.btnlogin.TabIndex = 7
+        Me.btnlogin.Text = "Login"
+        Me.btnlogin.UseVisualStyleBackColor = False
+        '
+        'cmdlogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnlogin)
+        Me.Controls.Add(Me.cbrmbr)
+        Me.Controls.Add(Me.tbpassword)
+        Me.Controls.Add(Me.tbusername)
+        Me.Controls.Add(Me.lbpassword)
+        Me.Controls.Add(Me.lbusername)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "cmdlogin"
+        Me.Text = "Login "
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents lbusername As System.Windows.Forms.Label
+    Friend WithEvents lbpassword As System.Windows.Forms.Label
+    Friend WithEvents tbusername As System.Windows.Forms.TextBox
+    Friend WithEvents tbpassword As System.Windows.Forms.TextBox
+    Friend WithEvents cbrmbr As System.Windows.Forms.CheckBox
+    Friend WithEvents btnlogin As System.Windows.Forms.Button
+End Class
